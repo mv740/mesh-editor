238e08fe5d4393ac300034e9681a46ee3febbf96
michal <michal@michalwozniak.ca>
2025-06-22 13:32:30 -0400

    test: add e2e vitest browser mode

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,9 +10,42 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   test: {
-    environment: 'happy-dom',
     globals: true,
     setupFiles: './tests/setup.ts',
+    coverage: {
+      reporter: ['text', 'html', 'json-summary', 'json', 'lcov'],
+      reportOnFailure: true,
+      include: ['src/**/*.{ts,tsx}'],
+    },
+    projects: [
+      // matches every folder and file inside the `packages` folder
+      'packages/*',
+      {
+        // add "extends: true" to inherit the options from the root config
+        extends: true,
+        test: {
+          include: ['tests/**/*.test.{ts,js}'],
+          // it is recommended to define a name when using inline configs
+          name: 'unit',
+          environment: 'happy-dom',
+        },
+      },
+      {
+        // add "extends: true" to inherit the options from the root config
+        extends: true,
+        test: {
+          include: ['tests/**/*.e2e.test.{ts,tsx}'],
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            // https://vitest.dev/guide/browser/playwright
+            instances: [{ browser: 'chromium' }],
+          },
+          name: 'e2e',
+        },
+      },
+    ],
   },
 })
