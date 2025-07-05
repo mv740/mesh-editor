a3cd2d74eda7a69a12dfc7b14e260e213ef7add7
michal <michal@michalwozniak.ca>
2025-07-05 10:54:55 -0400

    fix: tailwind import for playground and tests

--- tests/HelloWorld.e2e.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-import { render } from 'vitest-browser-react'
-import { Button } from '../src/index.js'
-
-test('renders name', async () => {
-  const { getByText } = render(<Button>Hello Vitest!</Button>)
-  await expect.element(getByText('Hello Vitest!')).toBeInTheDocument()
-})
