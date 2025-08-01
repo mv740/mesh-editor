64be838cf425f030f386df9962374d57add1cde1
michal <michal@wozniak.dev>
2025-08-01 17:30:40 -0400

    feat: add outline visiblity

--- src/components/editor/transform/transform-controls.tsx
+++ src/components/editor/transform/transform-controls.tsx
@@ -1,6 +1,16 @@
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
 import { Card, CardContent, CardHeader, CardTitle } from '../../ui/card'
 
-export const TransformControls = () => {
+type TransformControlsProps = {
+  meshOutlineVisible: boolean
+  setMeshOutlineVisible: (visible: boolean) => void
+}
+
+export const TransformControls = ({
+  meshOutlineVisible,
+  setMeshOutlineVisible,
+}: TransformControlsProps) => {
   return (
     <Card className="w-full max-w-xs sm:min-w-[350px] h-[200px] md:h-[300px] lg:h-[450px]">
       <CardHeader>
@@ -10,7 +20,16 @@ export const TransformControls = () => {
           </CardTitle>
         </div>
       </CardHeader>
-      <CardContent className="flex flex-col p-4 overflow-hidden"></CardContent>
+      <CardContent className="flex flex-col p-4 overflow-hidden">
+        <div className="flex items-center gap-2">
+          <Label htmlFor="mesh-outline-labels">Mesh Outline Labels:</Label>
+          <Switch
+            id="mesh-outline-labels"
+            checked={meshOutlineVisible}
+            onCheckedChange={setMeshOutlineVisible}
+          />
+        </div>
+      </CardContent>
     </Card>
   )
 }
