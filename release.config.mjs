0039d9372d58d67f85503059d7de6a13333a7fe0
michal <michal@wozniak.dev>
2025-09-25 22:46:58 -0400

    chore: add @semantic-release/git #9

--- release.config.mjs
+++ release.config.mjs
@@ -25,6 +25,14 @@ export default {
     ],
     '@semantic-release/npm',
     [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
       '@semantic-release/github',
       {
         assets: [{ path: 'dist/*.tgz', label: 'dist' }],
